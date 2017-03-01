3d5f86afedb58a3769d056d6d1912e61e5705dea
Lewy Blue <looeee@gmail.com>
2017-03-01 11:37:52 +0700

    Updated lightbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -335,6 +335,8 @@ var avalonbox = createCommonjsModule(function (module) {
         previous(e);
       } else if (e.keyCode == '39') {
         next(e);
+      } else if (e.key == 'Escape') {
+        hideOverlay(e);
       }
     }
 
@@ -507,7 +509,6 @@ var greedyNav = createCommonjsModule(function (module) {
 
 var Greedy = interopDefault(greedyNav);
 
-//Simple uuid function
 var uuid = function b(a) {
   return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b);
 };
--- assets/js/vendor/avalonbox/avalonbox.js
+++ assets/js/vendor/avalonbox/avalonbox.js
@@ -20,26 +20,25 @@ function _interopRequireWildcard( obj ) {
   if ( obj && obj.__esModule ) {
     return obj;
   }
-  let newObj = {};
+  const newObj = {};
   if ( obj != null ) {
-          for ( let key in obj ) {
-              if ( Object.prototype.hasOwnProperty.call( obj, key ) )
-                  {newObj[key] = obj[key];}
-            }
-        }
+    for ( const key in obj ) {
+      if ( Object.prototype.hasOwnProperty.call( obj, key ) ) { newObj[key] = obj[key]; }
+    }
+  }
   newObj.default = obj;
   return newObj;
 
 }
 
 const Avalonbox = ( function () {
-  let doc = document;
-  let buttons = {};
-  let overlay = html.createOverlayBox( doc );
-  let frame = html.createFrame( doc );
-  let spinner = html.createSpinner( doc );
-  let spinnerWrapper = html.createSpinnerWrapper( doc );
-  let downloadImage = new Image();
+  const doc = document;
+  const buttons = {};
+  const overlay = html.createOverlayBox( doc );
+  const frame = html.createFrame( doc );
+  const spinner = html.createSpinner( doc );
+  const spinnerWrapper = html.createSpinnerWrapper( doc );
+  const downloadImage = new Image();
 
   let active = void 0;
   let currentLink = void 0;
@@ -47,128 +46,121 @@ const Avalonbox = ( function () {
   initialize();
 
   function initialize() {
-      active = false;
-      html.hide( overlay );
-      html.appendChild( doc, overlay );
-      buttons.prev = html.createPreviousButton( doc );
-      buttons.next = html.createNextButton( doc );
-      spinnerWrapper.appendChild( spinner );
-      overlay.appendChild( frame.container );
-      overlay.appendChild( spinnerWrapper );
-      overlay.appendChild( buttons.prev );
-      overlay.appendChild( buttons.next );
+    active = false;
+    html.hide( overlay );
+    html.appendChild( doc, overlay );
+    buttons.prev = html.createPreviousButton( doc );
+    buttons.next = html.createNextButton( doc );
+    spinnerWrapper.appendChild( spinner );
+    overlay.appendChild( frame.container );
+    overlay.appendChild( spinnerWrapper );
+    overlay.appendChild( buttons.prev );
+    overlay.appendChild( buttons.next );
 
-      ( 0, _bind2.default )( overlay, 'click', hideOverlay );
-      ( 0, _bind2.default )( buttons.prev, 'click', previous );
-      ( 0, _bind2.default )( buttons.next, 'click', next );
-      ( 0, _bind2.default )( doc, 'keydown', keyPressHandler );
-    }
+    ( 0, _bind2.default )( overlay, 'click', hideOverlay );
+    ( 0, _bind2.default )( buttons.prev, 'click', previous );
+    ( 0, _bind2.default )( buttons.next, 'click', next );
+    ( 0, _bind2.default )( doc, 'keydown', keyPressHandler );
+  }
 
   function hideOverlay( e ) {
-      let f = frame.container;
-      if ( f === e.target || !f.contains( e.target ) )
-          {cleanFrame();}
-    }
+    const f = frame.container;
+    if ( f === e.target || !f.contains( e.target ) ) { cleanFrame(); }
+  }
 
   function cleanFrame() {
-      html.hide( overlay );
-      frame.image.src = '';
-      active = false;
-    }
+    html.hide( overlay );
+    frame.image.src = '';
+    active = false;
+  }
 
   function showOverlay( e ) {
-      e.preventDefault();
+    e.preventDefault();
 
-      active = true;
-      html.show( overlay );
-      currentLink = e.target.parentNode;
+    active = true;
+    html.show( overlay );
+    currentLink = e.target.parentNode;
 
-      loadImage();
+    loadImage();
 
-      if ( single( currentLink.parentNode.id ) ) {
-          html.hide( buttons.prev );
-          html.hide( buttons.next );
-        } else {
-          if ( currentLink.previousElementSibling && currentLink.previousElementSibling.tagName === 'A' )
-              {html.show(buttons.prev);}
-          else
-                {html.hide(buttons.prev);}
+    if ( single( currentLink.parentNode.id ) ) {
+      html.hide( buttons.prev );
+      html.hide( buttons.next );
+    } else {
+      if ( currentLink.previousElementSibling && currentLink.previousElementSibling.tagName === 'A' ) { html.show( buttons.prev ); } else { html.hide( buttons.prev ); }
 
-          if ( currentLink.nextElementSibling && currentLink.nextElementSibling.tagName === 'A' )
-              {html.show(buttons.next);}
-          else
-                {html.hide(buttons.next);}
-        }
+      if ( currentLink.nextElementSibling && currentLink.nextElementSibling.tagName === 'A' ) { html.show( buttons.next ); } else { html.hide( buttons.next ); }
     }
+  }
 
   function next( e ) {
-      html.show( buttons.prev );
-      if ( currentLink.nextElementSibling && currentLink.nextElementSibling.tagName === 'A' ) {
-          currentLink = currentLink.nextElementSibling;
-          loadImage();
-          if ( currentLink.previousElementSibling && currentLink.nextElementSibling.tagName !== 'A' )
-              {html.hide(buttons.next);}
-        }
-
-      e.stopPropagation();
+    html.show( buttons.prev );
+    if ( currentLink.nextElementSibling && currentLink.nextElementSibling.tagName === 'A' ) {
+      currentLink = currentLink.nextElementSibling;
+      loadImage();
+      if ( currentLink.previousElementSibling && currentLink.nextElementSibling.tagName !== 'A' ) { html.hide( buttons.next ); }
     }
+
+    e.stopPropagation();
+  }
 
   function previous( e ) {
-      html.show( buttons.next );
-      if ( currentLink.previousElementSibling && currentLink.previousElementSibling.tagName === 'A' ) {
-          currentLink = currentLink.previousElementSibling;
-          loadImage();
-          if ( currentLink.previousElementSibling && currentLink.previousElementSibling.tagName !== 'A' )
-              {html.hide(buttons.prev);}
-        }
-
-      e.stopPropagation();
+    html.show( buttons.next );
+    if ( currentLink.previousElementSibling && currentLink.previousElementSibling.tagName === 'A' ) {
+      currentLink = currentLink.previousElementSibling;
+      loadImage();
+      if ( currentLink.previousElementSibling && currentLink.previousElementSibling.tagName !== 'A' ) { html.hide( buttons.prev ); }
     }
+
+    e.stopPropagation();
+  }
 
   function loadImage() {
-      frame.image.src = '';
-      html.hide( frame.image );
-      html.show( spinner );
-      downloadImage.onload = function () {
-          html.show( frame.image );
-          frame.image.src = this.src;
-          html.hide( spinner );
-        };
+    frame.image.src = '';
+    html.hide( frame.image );
+    html.show( spinner );
+    downloadImage.onload = function () {
+      html.show( frame.image );
+      frame.image.src = this.src;
+      html.hide( spinner );
+    };
 
-      downloadImage.src = currentLink.getAttribute( 'href' );
-      frame.link.href = currentLink.getAttribute( 'href' );
-    }
+    downloadImage.src = currentLink.getAttribute( 'href' );
+    frame.link.href = currentLink.getAttribute( 'href' );
+  }
 
     // TODO: Swap [].slice for Array.from (ES6)
     // Need to test in IE9
   function single( query ) {
-      let links = doc.getElementById( query ).getElementsByTagName( 'a' );
-      return [].slice.call( links ).length == 1;
-    }
+    const links = doc.getElementById( query ).getElementsByTagName( 'a' );
+    return [].slice.call( links ).length == 1;
+  }
 
   function run( query ) {
-      eventHandlers( query );
-    }
+    eventHandlers( query );
+  }
 
   function eventHandlers( query ) {
-      let links = document.getElementById( query ).getElementsByTagName( 'a' );
-      links = [].slice.call( links );
-      links.forEach( (link) => {
-          ( 0, _bind2.default )( link, 'click', showOverlay );
-        } );
-    }
+    let links = document.getElementById( query ).getElementsByTagName( 'a' );
+    links = [].slice.call( links );
+    links.forEach( ( link ) => {
+      ( 0, _bind2.default )( link, 'click', showOverlay );
+    } );
+  }
 
   function keyPressHandler( e ) {
-      e = e || window.event;
+    e = e || window.event;
 
-      if ( !active )
-          {return;}
+    if ( !active ) { return; }
 
-      if ( e.keyCode == '37' )
-          {previous(e);}
-      else if ( e.keyCode == '39' )
-          {next(e);}
+    if ( e.keyCode == '37' ) { 
+      previous( e ); 
+    } else if ( e.keyCode == '39' ) { 
+      next( e ); 
+    } else if ( e.key == 'Escape' ) {
+      hideOverlay ( e );
     }
+  }
 
   return { run };
 }() );
