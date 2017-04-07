c344e0d8869d9ca521aaebabbcffb07cb0980e59
Lewy Blue <looeee@gmail.com>
2017-04-07 15:27:18 +0700

    Monkey patched THREE.Math.generateUUID

--- assets/js/src/3d/pages/work/escherSketch/escherSketchCanvasSetup.js
+++ assets/js/src/3d/pages/work/escherSketch/escherSketchCanvasSetup.js
@@ -1,7 +1,11 @@
-// import * as THREE from 'three';
+import * as THREE from 'three';
 
 import './utilities/polyfills.js';
 
 // Set up THREE
-// THREE.Cache.enabled = true;
+THREE.Cache.enabled = true;
 
+let uuid = 1;
+THREE.Math.generateUUID = function () {
+  return uuid++;
+};
